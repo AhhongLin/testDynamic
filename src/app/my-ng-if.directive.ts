c7b10fadea997d609457143bc82098d40ed17231
Ahhong <ahhong@miniasp.com>
2024-06-21 15:57:00 +0800

    加上 then else 功能

--- src/app/my-ng-if.directive.ts
+++ src/app/my-ng-if.directive.ts
@@ -1,18 +1,33 @@
-import {Directive, inject, Input, TemplateRef, ViewContainerRef} from '@angular/core';
+import {Directive, inject, Input, OnChanges, SimpleChanges, TemplateRef, ViewContainerRef} from '@angular/core';
 
 @Directive({
   selector: '[appMyNgIf]',
   standalone: true
 })
-export class MyNgIfDirective {
+export class MyNgIfDirective implements OnChanges{
   tRef = inject(TemplateRef);
   vcRef = inject(ViewContainerRef);
 
-  @Input() set appMyNgIf(condition: boolean) {
-    if (condition) {
-      this.vcRef.createEmbeddedView(this.tRef);
-    } else {
+  @Input() appMyNgIf?: boolean;
+  @Input() appMyNgIfThen?: TemplateRef<any>;
+  @Input() appMyNgIfElse?: TemplateRef<any>;
+
+  ngOnChanges(simpleChanges: SimpleChanges): void {
+    console.log(this.appMyNgIfThen);
+    console.log('ngOnChanges appMyNgIf', this.appMyNgIf);
+    if (simpleChanges['appMyNgIf']) {
       this.vcRef.clear();
+      if (this.appMyNgIf) {
+        if (this.appMyNgIfThen) {
+          this.vcRef.createEmbeddedView(this.appMyNgIfThen);
+        }else {
+          this.vcRef.createEmbeddedView(this.tRef);
+        }
+      } else if (!this.appMyNgIf) {
+        if (this.appMyNgIfElse) {
+          this.vcRef.createEmbeddedView(this.appMyNgIfElse);
+        }
+      }
     }
   }
 
